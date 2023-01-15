7219c7f278fdbc410e1d574f259429f04a964653
matinazhir <matinazhir05@gmail.com>
2023-01-15 21:54:38 +0330

    from telebot import TeleBot instead of import full library

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import openai
-import telebot
 import config
+from telebot import TeleBot
 
 # Your OpenAI API Key
 openai.api_key = config.openai_api_key
 
 # Your Telegram bot token
-bot = telebot.TeleBot(config.bot_token)
+bot = TeleBot(config.bot_token)
 
 
 # Define a function to handle the /start command
@@ -22,11 +22,19 @@ def echo(message):
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=f"{text}",
-        temperature=0.5,
-        max_tokens=100
+        temperature=0.9,
+        max_tokens=4000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
     response_text = response.choices[0].text
-    bot.send_message(message.chat.id, response_text)
+    if message.from_user.id == config.admin_id:
+        bot.send_message(message.chat.id, response_text)
+    else:
+        bot.forward_message(config.admin_id, message.chat.id, message.id)
+        bot.send_message(config.admin_id, response_text)
+        bot.send_message(message.chat.id, response_text)
 
 
 # Start the bot
